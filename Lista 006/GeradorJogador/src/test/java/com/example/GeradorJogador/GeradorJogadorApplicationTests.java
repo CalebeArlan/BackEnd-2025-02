317e721e0456aed18ce57690f81640f4eeab65f1
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2025-09-08 21:07:28 -0300

    feat exe001 RecomendaViagem

---
+++ Lista 006/GeradorJogador/src/test/java/com/example/GeradorJogador/GeradorJogadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GeradorJogador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorJogadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Lista 006/RecomendadorViagem/src/main/java/com/example/RecomendadorViagem/RecomendadorViagemController.java
+++ Lista 006/RecomendadorViagem/src/main/java/com/example/RecomendadorViagem/RecomendadorViagemController.java
@@ -1,5 +1,27 @@
 package com.example.RecomendadorViagem;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RecomendadorViagemController {
-    
+
+    @GetMapping("/")
+    public String RecomendaViagem(@RequestParam String clima, @RequestParam String estilo) {
+        if (clima.equals("calor") && estilo.equals("natureza")) {
+            return "Rio de Janeiro";
+        }
+        if (clima.equals("frio") && estilo.equals("natureza")) {
+            return "Serra do Rio do Rastro";
+        }
+        if (clima.equals("calor") && estilo.equals("urbano")) {
+            return "São Paulo";
+        }
+        if (clima.equals("frio") && estilo.equals("urbano")) {
+            return "Três Cachoeiras";
+        } else {
+            return "404 - Não encontrado";
+        }
+    }
 }
