b40537a6db668dd15565a9a32914852efb3ca017
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2025-09-25 00:35:47 -0300

    feat Exe09.01

---
+++ Lista 009/EXE0901/src/test/java/com/example/EXE0901/Exe0901ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EXE0901;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exe0901ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
